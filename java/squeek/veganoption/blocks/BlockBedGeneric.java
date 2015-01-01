48d0ff7e64e7c22e05264cb3002a35a2deb856d4
squeek <squeek502@hotmail.com>
2014-12-31 22:25:38 -0800

    Fix beds being unsleepable

--- java/squeek/veganoption/blocks/BlockBedGeneric.java
+++ java/squeek/veganoption/blocks/BlockBedGeneric.java
@@ -2,8 +2,10 @@ package squeek.veganoption.blocks;
 
 import java.util.Random;
 import net.minecraft.block.BlockBed;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.world.IBlockAccess;
 
 /**
  *  Necessary because Items.bed is hardcoded in BlockBed getItemDropped method
@@ -28,5 +30,11 @@ public class BlockBedGeneric extends BlockBed
 	public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
 	{
 		return isBlockHeadOfBed(p_149650_1_) ? Item.getItemById(0) : bedItem;
+	}
+
+	@Override
+	public boolean isBed(IBlockAccess world, int x, int y, int z, EntityLivingBase player)
+	{
+		return true;
 	}
 }
