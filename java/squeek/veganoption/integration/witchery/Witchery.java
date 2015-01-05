b8c7ac6072bb098567534980074ec701c1d39e1d
squeek <squeek502@hotmail.com>
2015-01-04 22:39:09 -0800

    Witchery wood ash integration

--- java/squeek/veganoption/integration/witchery/Witchery.java
+++ java/squeek/veganoption/integration/witchery/Witchery.java
@@ -1,10 +1,16 @@
 package squeek.veganoption.integration.witchery;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 import squeek.veganoption.integration.IIntegrator;
+import squeek.veganoption.integration.IntegrationHandler;
+import squeek.veganoption.registry.Content;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Witchery implements IIntegrator
 {
-	// TODO: Wood Ash
+	public static final int woodAshMetadata = 18;
 
 	@Override
 	public void preInit()
@@ -14,6 +20,12 @@ public class Witchery implements IIntegrator
 	@Override
 	public void init()
 	{
+		Item ingredient = GameRegistry.findItem(IntegrationHandler.MODID_WITCHERY, "ingredient");
+		if (ingredient != null)
+		{
+			ItemStack woodAsh = new ItemStack(ingredient, 1, woodAshMetadata);
+			OreDictionary.registerOre(Content.woodAshOreDict, woodAsh);
+		}
 	}
 
 	@Override
