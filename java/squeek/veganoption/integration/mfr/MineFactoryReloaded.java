2541a0a0da4330feffb644ecc5c6a05bbd5d23a2
squeek <squeek502@hotmail.com>
2015-01-04 22:39:30 -0800

    MFR fertilizer integration

--- java/squeek/veganoption/integration/mfr/MineFactoryReloaded.java
+++ java/squeek/veganoption/integration/mfr/MineFactoryReloaded.java
@@ -1,12 +1,15 @@
 package squeek.veganoption.integration.mfr;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import squeek.veganoption.integration.IIntegrator;
+import squeek.veganoption.integration.IntegrationHandler;
+import squeek.veganoption.registry.Content;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class MineFactoryReloaded implements IIntegrator
 {
-	// TODO: IMC "registerFertilizer" with Content.fertilizer
-	// TODO: Resolve plastic TiC conflicts
-
 	@Override
 	public void preInit()
 	{
@@ -15,10 +18,47 @@ public class MineFactoryReloaded implements IIntegrator
 	@Override
 	public void init()
 	{
+		registerFertilizer(new ItemStack(Content.fertilizer), FertilizerType.GrowPlant);
 	}
 
 	@Override
 	public void postInit()
 	{
+	}
+
+	// copied from powercrystals.minefactoryreloaded.api.FertilizerType
+	public static enum FertilizerType
+	{
+		/**
+		* The fertilizer will fertilize nothing.
+		*/
+		None,
+		/**
+		* The fertilizer will fertilize grass.
+		*/
+		Grass,
+		/**
+		* The fertilizer will grow a plant.
+		*/
+		GrowPlant,
+		/**
+		* The fertilizer will grow magical crops.
+		*/
+		GrowMagicalCrop,
+	}
+
+	/**
+	 * registerFertilizer_Standard | An NBTTag with the fert (Item, String identifier), meta (Integer), and
+	 * type (Integer, index into FertilizerType.values()) attributes set.
+	 */
+	public void registerFertilizer(ItemStack itemStack, FertilizerType type)
+	{
+		NBTTagCompound toSend = new NBTTagCompound();
+
+		toSend.setString("fert", Item.itemRegistry.getNameForObject(itemStack.getItem()));
+		toSend.setInteger("meta", itemStack.getItemDamage());
+		toSend.setInteger("type", type.ordinal());
+
+		FMLInterModComms.sendMessage(IntegrationHandler.MODID_MINEFACTORY_RELOADED, "registerFertilizer_Standard", toSend);
 	}
 }
