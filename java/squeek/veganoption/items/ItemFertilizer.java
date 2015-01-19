4a43239f0bf87a46f55786379d3baa2f0682087f
squeek <squeek502@hotmail.com>
2015-01-18 22:10:03 -0800

    Add dispenser behavior to fertilizer

--- java/squeek/veganoption/items/ItemFertilizer.java
+++ java/squeek/veganoption/items/ItemFertilizer.java
@@ -1,9 +1,13 @@
 package squeek.veganoption.items;
 
+import net.minecraft.block.BlockDispenser;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 public class ItemFertilizer extends Item
@@ -11,6 +15,7 @@ public class ItemFertilizer extends Item
 	public ItemFertilizer()
 	{
 		super();
+		BlockDispenser.dispenseBehaviorRegistry.putObject(this, new ItemFertilizer.DispenserBehavior());
 	}
 
 	@Override
@@ -25,5 +30,49 @@ public class ItemFertilizer extends Item
 			return true;
 		}
 		return false;
+	}
+
+	// copied from bonemeal's dispenser behavior in net.minecraft.init.Bootstrap
+	public static class DispenserBehavior extends BehaviorDefaultDispenseItem
+	{
+		private boolean didFertilize = true;
+
+		@Override
+		protected ItemStack dispenseStack(IBlockSource blockSource, ItemStack itemStack)
+		{
+			EnumFacing enumfacing = BlockDispenser.func_149937_b(blockSource.getBlockMetadata());
+			World world = blockSource.getWorld();
+			int x = blockSource.getXInt() + enumfacing.getFrontOffsetX();
+			int y = blockSource.getYInt() + enumfacing.getFrontOffsetY();
+			int z = blockSource.getZInt() + enumfacing.getFrontOffsetZ();
+
+			if (ItemDye.func_150919_a(itemStack, world, x, y, z))
+			{
+				if (!world.isRemote)
+				{
+					world.playAuxSFX(2005, x, y, z, 0);
+				}
+				this.didFertilize = true;
+			}
+			else
+			{
+				this.didFertilize = false;
+			}
+
+			return itemStack;
+		}
+
+		@Override
+		protected void playDispenseSound(IBlockSource blockSource)
+		{
+			if (this.didFertilize)
+			{
+				blockSource.getWorld().playAuxSFX(1000, blockSource.getXInt(), blockSource.getYInt(), blockSource.getZInt(), 0);
+			}
+			else
+			{
+				blockSource.getWorld().playAuxSFX(1001, blockSource.getXInt(), blockSource.getYInt(), blockSource.getZInt(), 0);
+			}
+		}
 	}
 }
