3decb4456242ea545bbc456d6d00a91b18da59fa
squeek <squeek502@hotmail.com>
2015-01-24 21:54:53 -0800

    Add integration with Still Hungry!'s rice, oil, grape seeds

--- java/squeek/veganoption/integration/stillhungry/StillHungry.java
+++ java/squeek/veganoption/integration/stillhungry/StillHungry.java
@@ -5,12 +5,25 @@ import java.util.HashSet;
 import java.util.Set;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+import squeek.veganoption.content.ContentHelper;
 import squeek.veganoption.content.registry.CompostRegistry;
 import squeek.veganoption.content.registry.CompostRegistry.FoodSpecifier;
 import squeek.veganoption.integration.IntegratorBase;
 
 public class StillHungry extends IntegratorBase
 {
+	@Override
+	public void oredict()
+	{
+		if (getItem("StillHungry_rice") != null)
+			OreDictionary.registerOre(ContentHelper.riceOreDict, new ItemStack(getItem("StillHungry_rice")));
+		if (getItem("StillHungry_oil") != null)
+			OreDictionary.registerOre(ContentHelper.vegetableOilOreDict, new ItemStack(getItem("StillHungry_oil")));
+		if (getItem("StillHungry_grapeSeed") != null)
+			OreDictionary.registerOre(ContentHelper.grapeSeedOreDict, new ItemStack(getItem("StillHungry_grapeSeed")));
+	}
+
 	@Override
 	public void init()
 	{
