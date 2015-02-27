17988ac5fd4e267b044f62064a3725a623a349d3
squeek <squeek502@hotmail.com>
2015-02-26 21:26:47 -0800

    Add piston crafting recipes for various HarvestCraft items  * Adds recipes for: Mashed potatoes, batter, dough, and all fruit juices

--- java/squeek/veganoption/integration/pams/HarvestCraft.java
+++ java/squeek/veganoption/integration/pams/HarvestCraft.java
@@ -1,17 +1,26 @@
 package squeek.veganoption.integration.pams;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 import squeek.veganoption.ModInfo;
 import squeek.veganoption.VeganOption;
 import squeek.veganoption.content.ContentHelper;
 import squeek.veganoption.content.Modifiers;
+import squeek.veganoption.content.recipes.InputItemStack;
+import squeek.veganoption.content.recipes.PistonCraftingRecipe;
 import squeek.veganoption.content.registry.CompostRegistry;
 import squeek.veganoption.content.registry.CompostRegistry.FoodSpecifier;
+import squeek.veganoption.content.registry.PistonCraftingRegistry;
 import squeek.veganoption.helpers.TooltipHelper;
 import squeek.veganoption.integration.IntegratorBase;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -52,6 +61,10 @@ public class HarvestCraft extends IntegratorBase
 	{
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(bbqSauce), "toolSaucepan", "foodKetchup", "foodVinegar", Items.sugar, "foodMustard", "listAllwater", "foodSalt", "foodBlackpepper"));
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(bbqTofu), "toolBakeware", "foodFirmtofu", ContentHelper.bbqSauceOreDict));
+
+		PistonCraftingRegistry.register(new PistonCraftingRecipe(getItem("doughItem"), FluidRegistry.WATER, "foodFlour", "foodSalt"));
+		PistonCraftingRegistry.register(new PistonCraftingRecipe(getItem("batterItem"), "foodFlour", ContentHelper.eggBakingOreDict));
+		PistonCraftingRegistry.register(new PistonCraftingRecipe(getItem("mashedpotatoesItem"), "foodButteredpotato", "foodSalt"));
 
 		// exclude non-baked goods from egg replacer conversion
 		final String[] foodNamesToExclude = new String[]
@@ -101,5 +114,50 @@ public class HarvestCraft extends IntegratorBase
 				return false;
 			}
 		});
+	}
+
+	@Override
+	public void postInit()
+	{
+		super.postInit();
+
+		// do this late because I think HarvestCraft registers its recipes late
+		for (ItemStack juice : OreDictionary.getOres("listAlljuice"))
+		{
+			if (juice.getItem() == null || juice.getItem().hasContainerItem(juice))
+				continue;
+
+			List<InputItemStack> juiceInputs = new ArrayList<InputItemStack>();
+
+			@SuppressWarnings("unchecked")
+			List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();
+			for (IRecipe recipe : recipes)
+			{
+				if (recipe.getRecipeOutput() != null && OreDictionary.itemMatches(juice, recipe.getRecipeOutput(), false))
+				{
+					@SuppressWarnings("rawtypes")
+					List ingredients = null;
+
+					if (recipe instanceof ShapelessRecipes)
+						ingredients = ((ShapelessRecipes) recipe).recipeItems;
+					else if (recipe instanceof ShapelessOreRecipe)
+						ingredients = ((ShapelessOreRecipe) recipe).getInput();
+					else
+						continue;
+
+					for (Object ingredient : ingredients)
+					{
+						if (ingredient instanceof ItemStack && ((ItemStack) ingredient).getItem() == getItem("juicerItem"))
+							continue;
+						else if (ingredient instanceof ArrayList && ingredient == OreDictionary.getOres("toolJuicer"))
+							continue;
+
+						juiceInputs.add(new InputItemStack(ingredient));
+					}
+				}
+			}
+
+			PistonCraftingRegistry.register(new PistonCraftingRecipe(juice.copy(), juiceInputs.toArray()));
+		}
 	}
 }
