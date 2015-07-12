100db5dfeb8d74bd9a5d77e0c8a98c596a6f96ef
squeek <squeek502@hotmail.com>
2015-07-11 22:23:37 -0700

    Add temperature gauge overlay to composter model  * Contributes towards #20
    (cherry picked from commit d7815e7774b262390cea964f8ed5b64f6cc86c74)

--- java/squeek/veganoption/blocks/renderers/RenderComposter.java
+++ java/squeek/veganoption/blocks/renderers/RenderComposter.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.util.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 import squeek.veganoption.ModInfo;
@@ -20,6 +21,12 @@ public class RenderComposter extends TileEntitySpecialRenderer implements IItemR
 	private static final ResourceLocation TEXTURE_CHRISTMAS = new ResourceLocation("textures/entity/chest/christmas.png");
 	private static final ResourceLocation TEXTURE_NORMAL = new ResourceLocation("textures/entity/chest/normal.png");
 	private static final ResourceLocation TEXTURE_LEGS = new ResourceLocation(ModInfo.MODID_LOWER, "textures/entity/composter_legs.png");
+	private static final ResourceLocation[] TEXTURE_TEMPERATURE_OVERLAYS = new ResourceLocation[]{
+	new ResourceLocation(ModInfo.MODID_LOWER, "textures/entity/composter/temperature_overlay_blue.png"),
+	new ResourceLocation(ModInfo.MODID_LOWER, "textures/entity/composter/temperature_overlay_yellow.png"),
+	new ResourceLocation(ModInfo.MODID_LOWER, "textures/entity/composter/temperature_overlay_orange.png"),
+	new ResourceLocation(ModInfo.MODID_LOWER, "textures/entity/composter/temperature_overlay_red.png")
+	};
 	private ModelChest modelChest = new ModelChest();
 	private ModelComposterLegs modelLegs = new ModelComposterLegs();
 	private boolean isChristmas;
@@ -67,28 +74,36 @@ public class RenderComposter extends TileEntitySpecialRenderer implements IItemR
 
 		Axis axis = Math.abs(rotation) == 90 ? Axis.X : Axis.Y;
 
-		this.bindTexture(TEXTURE_LEGS);
+		if (MinecraftForgeClient.getRenderPass() <= 0)
+		{
+			this.bindTexture(TEXTURE_LEGS);
 
-		GL11.glPushMatrix();
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		GL11.glTranslatef((float) x + 0.5f, (float) y + 1.5F, (float) z + 0.5f);
-		GL11.glScalef(1F, -1F, -1F);
-		GL11.glRotatef((float) rotation, 0.0F, 1.0F, 0.0F);
+			GL11.glPushMatrix();
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glTranslatef((float) x + 0.5f, (float) y + 1.5F, (float) z + 0.5f);
+			GL11.glScalef(1F, -1F, -1F);
+			GL11.glRotatef((float) rotation, 0.0F, 1.0F, 0.0F);
 
-		modelLegs.renderAll();
+			modelLegs.renderAll();
 
-		GL11.glPopMatrix();
+			GL11.glPopMatrix();
+		}
 
 		ModelChest modelchest = this.modelChest;
 
-		if (this.isChristmas)
+		if (MinecraftForgeClient.getRenderPass() <= 0)
 		{
-			this.bindTexture(TEXTURE_CHRISTMAS);
+			if (this.isChristmas)
+			{
+				this.bindTexture(TEXTURE_CHRISTMAS);
+			}
+			else
+			{
+				this.bindTexture(TEXTURE_NORMAL);
+			}
 		}
 		else
-		{
-			this.bindTexture(TEXTURE_NORMAL);
-		}
+			this.bindTexture(TEXTURE_TEMPERATURE_OVERLAYS[getOverlayIndexFromTemperature(tile.getCompostTemperature())]);
 
 		GL11.glPushMatrix();
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
@@ -161,5 +176,19 @@ public class RenderComposter extends TileEntitySpecialRenderer implements IItemR
 			default:
 				return;
 		}
+	}
+
+	public int getOverlayIndexFromTemperature(float temperature)
+	{
+		temperature = Math.round(temperature);
+
+		if (temperature >= TileEntityComposter.MAX_COMPOST_TEMPERATURE)
+			return 3;
+		else if (temperature >= TileEntityComposter.THERMOPHILIC_RANGE_START)
+			return 2;
+		else if (temperature >= TileEntityComposter.MESOPHILIC_RANGE_START)
+			return 1;
+		else
+			return 0;
 	}
 }
