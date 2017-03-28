a42ab5ef1c668b5c4ce8fcd78c000f4bd592e7cf
elifoster <elifosterwy@gmail.com>
2017-03-27 18:28:06 -0700

    Add Thermal Expansion Frozen Bubble Transposer recipes

--- java/squeek/veganoption/integration/cofh/ThermalExpansion.java
+++ java/squeek/veganoption/integration/cofh/ThermalExpansion.java
@@ -3,8 +3,13 @@ package squeek.veganoption.integration.cofh;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import squeek.veganoption.content.modules.Egg;
+import squeek.veganoption.content.modules.Ender;
+import squeek.veganoption.content.modules.FrozenBubble;
 import squeek.veganoption.integration.IntegratorBase;
 
 public class ThermalExpansion extends IntegratorBase
@@ -13,6 +18,9 @@ public class ThermalExpansion extends IntegratorBase
 	public void init()
 	{
 		super.init();
+
+		addTransposerFill(4000, new ItemStack(FrozenBubble.frozenBubble), new ItemStack(Items.ENDER_PEARL), new FluidStack(Ender.fluidRawEnder, Ender.RAW_ENDER_PER_PEARL), true);
+		addTransposerFill(4000, new ItemStack(FrozenBubble.frozenBubble), new ItemStack(Items.ENDER_PEARL), FluidRegistry.getFluidStack("ender", FluidContainerRegistry.BUCKET_VOLUME / 4), false);
 
 		addPulverizerRecipe(1600, new ItemStack(Items.POTATO), new ItemStack(Egg.potatoStarch, 2), null, 100);
 	}
@@ -40,5 +48,27 @@ public class ThermalExpansion extends IntegratorBase
 		}
 
 		FMLInterModComms.sendMessage(MODID_THERMAL_EXPANSION, "addpulverizerrecipe", toSend);
+	}
+
+	public static void addTransposerFill(int energy, ItemStack input, ItemStack output, FluidStack fluid, boolean isReversible)
+	{
+		NBTTagCompound toSend = new NBTTagCompound();
+
+		toSend.setInteger("energy", energy);
+		toSend.setBoolean("reversible", isReversible);
+
+		NBTTagCompound inputTag = new NBTTagCompound();
+		inputTag = input.writeToNBT(inputTag);
+		toSend.setTag("input", inputTag);
+
+		NBTTagCompound outputTag = new NBTTagCompound();
+		outputTag = output.writeToNBT(outputTag);
+		toSend.setTag("output", outputTag);
+
+		NBTTagCompound fluidTag = new NBTTagCompound();
+		fluidTag = fluid.writeToNBT(fluidTag);
+		toSend.setTag("fluid", fluidTag);
+
+		FMLInterModComms.sendMessage(MODID_THERMAL_EXPANSION, "addtransposerfillrecipe", toSend);
 	}
 }
