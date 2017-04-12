20e3149fa6ee98a3cdd3654f944363bf7d76687d
elifoster <elifosterwy@gmail.com>
2017-04-11 22:17:00 -0700

    Fix onPlayerInteract being hand-specific

--- java/squeek/veganoption/helpers/FluidContainerHelper.java
+++ java/squeek/veganoption/helpers/FluidContainerHelper.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
@@ -70,21 +69,17 @@ public class FluidContainerHelper
 		if (event.isCanceled() || event.getResult() != Event.Result.DEFAULT)
 			return;
 
-		// TODO: Support for the OFF_HAND?
 		EntityPlayer player = event.getEntityPlayer();
-		if (player.getHeldItem(EnumHand.MAIN_HAND) == null)
+		ItemStack heldItem = event.getItemStack();
+		if (heldItem == null || heldItem.getItem() != Items.GLASS_BOTTLE)
 			return;
 
-		if (player.getHeldItem(EnumHand.MAIN_HAND).getItem() != Items.GLASS_BOTTLE)
-			return;
-
-		ItemStack emptyContainer = player.getHeldItem(EnumHand.MAIN_HAND);
 		World world = event.getWorld();
 
 		RayTraceResult rayTraceResult = null;
 		try
 		{
-			rayTraceResult = (RayTraceResult) rayTraceMethod.invoke(emptyContainer.getItem(), world, player, true);
+			rayTraceResult = (RayTraceResult) rayTraceMethod.invoke(heldItem.getItem(), world, player, true);
 		}
 		catch (RuntimeException e)
 		{
@@ -118,11 +113,11 @@ public class FluidContainerHelper
 		if (!world.canMineBlockBody(event.getEntityPlayer(), pos))
 			return;
 
-		if (!event.getEntityPlayer().canPlayerEdit(pos, rayTraceResult.sideHit, emptyContainer))
+		if (!event.getEntityPlayer().canPlayerEdit(pos, rayTraceResult.sideHit, heldItem))
 			return;
 
 		FluidStack fluidStack = new FluidStack(((BlockFluidClassic) block).getFluid(), Fluid.BUCKET_VOLUME);
-		boolean didFill = fillContainer(fluidStack, emptyContainer) > 0;
+		boolean didFill = fillContainer(fluidStack, heldItem) > 0;
 
 		// this cancels the interaction if the bottle is unable to be filled with the fluid,
 		// which stops mod fluid blocks from creating water bottles, because all fluids *have* to use
