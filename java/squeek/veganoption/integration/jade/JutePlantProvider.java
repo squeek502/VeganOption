a8ac2156d199343ae2abdddaa95103ae3a76e730
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2023-12-24 12:19:10 -0800

    Remove jute plant mature tooltip, as this should never happen A mature jute plant is just a large fern. If it ever reads 100%, something is probably going wrong.

--- java/squeek/veganoption/integration/jade/JutePlantProvider.java
+++ java/squeek/veganoption/integration/jade/JutePlantProvider.java
@@ -1,16 +1,13 @@
 package squeek.veganoption.integration.jade;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IBlockComponentProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.Identifiers;
 import snownee.jade.api.config.IPluginConfig;
-import snownee.jade.api.theme.IThemeHelper;
 import squeek.veganoption.ModInfo;
 import squeek.veganoption.blocks.BlockJutePlant;
-import squeek.veganoption.helpers.LangHelper;
 
 public class JutePlantProvider implements IBlockComponentProvider
 {
@@ -32,10 +29,7 @@ public class JutePlantProvider implements IBlockComponentProvider
 		if (config.get(Identifiers.MC_CROP_PROGRESS))
 		{
 			float growthValue = ((BlockJutePlant) accessor.getBlock()).getGrowthPercent(accessor.getLevel(), accessor.getPosition(), accessor.getBlockState());
-			if (growthValue < 1f)
-				VeganOptionPlugin.addPercentInfoToTooltip(tooltip, "waila.jute_growth", growthValue);
-			else
-				tooltip.add(Component.translatable(LangHelper.prependModId("waila.jute_growth"), IThemeHelper.get().success("waila.jute_growth.mature")));
+			VeganOptionPlugin.addPercentInfoToTooltip(tooltip, "waila.jute_growth", growthValue);
 		}
 	}
 
